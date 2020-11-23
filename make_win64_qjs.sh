1268c9104fad8e3615172508e495bd8951dbc215
johnche <johnche@tencent.com>
2020-11-23 18:05:44 +0800

    sh的语法错误

--- make_win64_qjs.sh
+++ make_win64_qjs.sh
@@ -1,5 +1,5 @@
-mkdir build_qjs & pushd build_qjs
+mkdir build_qjs & cd build_qjs
 cmake -DJS_ENGINE=quickjs -DLIB_NAME=quickjs -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "CodeBlocks - Unix Makefiles" ..
-popd
+cd ..
 cmake --build build_qjs --config Release
 pause
