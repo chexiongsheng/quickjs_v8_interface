c34301b186966e6195e88b673b12fff9c2607d48
johnche <johnche@tencent.com>
2020-11-23 20:19:42 +0800

    打印下信息

--- make_win64_qjs.sh
+++ make_win64_qjs.sh
@@ -3,4 +3,5 @@ cd build_qjs
 cmake -DCMAKE_CXX_COMPILER=g++ -DJS_ENGINE=quickjs -DLIB_NAME=quickjs -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "CodeBlocks - Unix Makefiles" ..
 cd ..
 cmake --build build_qjs --config Release
-
+ls build_qjs
+ls build_qjs/Release
