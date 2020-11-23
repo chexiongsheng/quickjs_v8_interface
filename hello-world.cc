2cd82bfbcefd2bc9846b1f6a8129040e57b80d00
johnche <johnche@tencent.com>
2020-11-23 21:51:42 +0800

    格式化

--- hello-world.cc
+++ hello-world.cc
@@ -28,91 +28,91 @@
 #endif
 
 int main(int argc, char* argv[]) {
-  // Initialize V8.
-  v8::StartupData SnapshotBlob;
-  SnapshotBlob.data = (const char *)SnapshotBlobCode;
-  SnapshotBlob.raw_size = sizeof(SnapshotBlobCode);
-  v8::V8::SetSnapshotDataBlob(&SnapshotBlob);
+    // Initialize V8.
+    v8::StartupData SnapshotBlob;
+    SnapshotBlob.data = (const char *)SnapshotBlobCode;
+    SnapshotBlob.raw_size = sizeof(SnapshotBlobCode);
+    v8::V8::SetSnapshotDataBlob(&SnapshotBlob);
 
-  std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
-  v8::V8::InitializePlatform(platform.get());
-  v8::V8::Initialize();
+    std::unique_ptr<v8::Platform> platform = v8::platform::NewDefaultPlatform();
+    v8::V8::InitializePlatform(platform.get());
+    v8::V8::Initialize();
 
-  // Create a new Isolate and make it the current one.
-  v8::Isolate::CreateParams create_params;
-  create_params.array_buffer_allocator =
-      v8::ArrayBuffer::Allocator::NewDefaultAllocator();
-  v8::Isolate* isolate = v8::Isolate::New(create_params);
-  {
-    v8::Isolate::Scope isolate_scope(isolate);
-
-    // Create a stack-allocated handle scope.
-    v8::HandleScope handle_scope(isolate);
-
-    // Create a new context.
-    v8::Local<v8::Context> context = v8::Context::New(isolate);
-
-    // Enter the context for compiling and running the hello world script.
-    v8::Context::Scope context_scope(context);
-
+    // Create a new Isolate and make it the current one.
+    v8::Isolate::CreateParams create_params;
+    create_params.array_buffer_allocator =
+        v8::ArrayBuffer::Allocator::NewDefaultAllocator();
+    v8::Isolate* isolate = v8::Isolate::New(create_params);
     {
-      // Create a string containing the JavaScript source code.
-      v8::Local<v8::String> source =
-          v8::String::NewFromUtf8(isolate, "'Hello' + ', World!'",
-                                  v8::NewStringType::kNormal)
-              .ToLocalChecked();
+        v8::Isolate::Scope isolate_scope(isolate);
 
-      // Compile the source code.
-      v8::Local<v8::Script> script =
-          v8::Script::Compile(context, source).ToLocalChecked();
+        // Create a stack-allocated handle scope.
+        v8::HandleScope handle_scope(isolate);
 
-      // Run the script to get the result.
-      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+        // Create a new context.
+        v8::Local<v8::Context> context = v8::Context::New(isolate);
 
-      // Convert the result to an UTF8 string and print it.
-      v8::String::Utf8Value utf8(isolate, result);
-      printf("%s\n", *utf8);
-    }
+        // Enter the context for compiling and running the hello world script.
+        v8::Context::Scope context_scope(context);
 
-    {
-      // Use the JavaScript API to generate a WebAssembly module.
-      //
-      // |bytes| contains the binary format for the following module:
-      //
-      //     (func (export "add") (param i32 i32) (result i32)
-      //       get_local 0
-      //       get_local 1
-      //       i32.add)
-      //
-      const char* csource = R"(
-        function add(x, y) {
-            return x + y;
+        {
+            // Create a string containing the JavaScript source code.
+            v8::Local<v8::String> source =
+                v8::String::NewFromUtf8(isolate, "'Hello' + ', World!'",
+                    v8::NewStringType::kNormal)
+                .ToLocalChecked();
+
+            // Compile the source code.
+            v8::Local<v8::Script> script =
+                v8::Script::Compile(context, source).ToLocalChecked();
+
+            // Run the script to get the result.
+            v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+
+            // Convert the result to an UTF8 string and print it.
+            v8::String::Utf8Value utf8(isolate, result);
+            printf("%s\n", *utf8);
         }
-        add(3, 4);
-      )";
 
-      // Create a string containing the JavaScript source code.
-      v8::Local<v8::String> source =
-          v8::String::NewFromUtf8(isolate, csource, v8::NewStringType::kNormal)
-              .ToLocalChecked();
+        {
+            // Use the JavaScript API to generate a WebAssembly module.
+            //
+            // |bytes| contains the binary format for the following module:
+            //
+            //     (func (export "add") (param i32 i32) (result i32)
+            //       get_local 0
+            //       get_local 1
+            //       i32.add)
+            //
+            const char* csource = R"(
+                function add(x, y) {
+                    return x + y;
+                }
+                add(3, 4);
+              )";
 
-      // Compile the source code.
-      v8::Local<v8::Script> script =
-          v8::Script::Compile(context, source).ToLocalChecked();
+            // Create a string containing the JavaScript source code.
+            v8::Local<v8::String> source =
+                v8::String::NewFromUtf8(isolate, csource, v8::NewStringType::kNormal)
+                .ToLocalChecked();
 
-      // Run the script to get the result.
-      v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+            // Compile the source code.
+            v8::Local<v8::Script> script =
+                v8::Script::Compile(context, source).ToLocalChecked();
 
-      // Convert the result to a uint32 and print it.
-      uint32_t number = result->Uint32Value(context).ToChecked();
-      printf("3 + 4 = %u\n", number);
+            // Run the script to get the result.
+            v8::Local<v8::Value> result = script->Run(context).ToLocalChecked();
+
+            // Convert the result to a uint32 and print it.
+            uint32_t number = result->Uint32Value(context).ToChecked();
+            printf("3 + 4 = %u\n", number);
+        }
     }
-  }
 
-  // Dispose the isolate and tear down V8.
-  isolate->Dispose();
-  v8::V8::Dispose();
-  v8::V8::ShutdownPlatform();
-  delete create_params.array_buffer_allocator;
-  return 0;
+    // Dispose the isolate and tear down V8.
+    isolate->Dispose();
+    v8::V8::Dispose();
+    v8::V8::ShutdownPlatform();
+    delete create_params.array_buffer_allocator;
+    return 0;
 }
