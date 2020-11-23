4c3ecd0f3408b0fdff9a89c36a4bcb4f46fcf00a
johnche <johnche@tencent.com>
2020-11-23 20:24:51 +0800

    上传目录

--- make_osx_qjs.sh
+++ make_osx_qjs.sh
@@ -2,5 +2,3 @@ mkdir -p build_qjs && cd build_qjs
 cmake -DJS_ENGINE=quickjs -DLIB_NAME=quickjs -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON  -GXcode ../
 cd ..
 cmake --build build_qjs --config Release
-ls build_qjs
-ls build_qjs/Release
