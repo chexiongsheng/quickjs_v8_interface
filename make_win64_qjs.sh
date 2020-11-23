1d3e795672338bebc252ea9255c8d66355578bc3
johnche <johnche@tencent.com>
2020-11-23 18:08:59 +0800

    脚本错误

--- make_win64_qjs.sh
+++ make_win64_qjs.sh
@@ -1,4 +1,5 @@
-mkdir build_qjs & cd build_qjs
+mkdir build_qjs
+cd build_qjs
 cmake -DJS_ENGINE=quickjs -DLIB_NAME=quickjs -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "CodeBlocks - Unix Makefiles" ..
 cd ..
 cmake --build build_qjs --config Release
